f678696c43f307fce9f3eb3f4738f852fc81368e
wangyu <wyu@yitong.com.cn>
2019-01-06 06:05:45 +0800

    2

---
+++ JFKit/JFKit.h
@@ -0,0 +1,19 @@
+//
+//  JFKit.h
+//  JFKit
+//
+//  Created by 王俣 on 2019/1/6.
+//  Copyright © 2019 王俣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JFKit.
+FOUNDATION_EXPORT double JFKitVersionNumber;
+
+//! Project version string for JFKit.
+FOUNDATION_EXPORT const unsigned char JFKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFKit/PublicHeader.h>
+
+
